ecdef3638122ec9d3dcb7987aff7adf7e4642901
fengkuangdeshitou <409744573@qq.com>
2022-07-05 16:31:47 +0800

    删除UIWebView

--- Pods/Target Support Files/iOS-Echarts/iOS-Echarts-umbrella.h
+++ Pods/Target Support Files/iOS-Echarts/iOS-Echarts-umbrella.h
@@ -64,8 +64,6 @@
 #import "PYWordCloudSeries.h"
 #import "PYUtilities.h"
 #import "PYJsonUtil.h"
-#import "PYEchartsView.h"
-#import "PYZoomEchartsView.h"
 #import "WKEchartsView.h"
 
 FOUNDATION_EXPORT double iOS_EchartsVersionNumber;
