306dfbb399761da5c9ea64b5149bac1ed3cf082c
fengkuangdeshitou <409744573@qq.com>
2022-07-16 09:40:27 +0800

    合并

---
+++ ReservoirMonitoring/BLE/BlueListView.h
@@ -0,0 +1,16 @@
+//
+//  BlueListView.h
+//  ReservoirMonitoring
+//
+//  Created by maiyou on 2022/7/15.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BlueListView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ReservoirMonitoring/BLE/BlueListView.m
@@ -0,0 +1,20 @@
+//
+//  BlueListView.m
+//  ReservoirMonitoring
+//
+//  Created by maiyou on 2022/7/15.
+//
+
+#import "BlueListView.h"
+
+@implementation BlueListView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
